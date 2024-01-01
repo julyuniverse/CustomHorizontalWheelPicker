3da675522c4a5c6dca79861738302a75a3faba3b
Lee Taesung <julyuniverse@naver.com>
2024-01-01 23:14:33 +0900

    Initial Commit

---
+++ CustomHorizontalWheelPicker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CustomHorizontalWheelPicker
+//
+//  Created by July universe on 1/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CustomHorizontalWheelPicker/CustomHorizontalWheelPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomHorizontalWheelPickerApp.swift
+//  CustomHorizontalWheelPicker
+//
+//  Created by July universe on 1/1/24.
+//
+
+import SwiftUI
+
+@main
+struct CustomHorizontalWheelPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
